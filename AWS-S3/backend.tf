9ede9b029e7f7bf904e568a60bf1fc084dd799e9
SIDVJSINGH <sidvjsingh@gmail.com>
2023-09-08 18:49:22 +0530

    backend removed

--- AWS-S3/backend.tf
+++ AWS-S3/backend.tf
@@ -12,10 +12,12 @@ provider "aws" {
   secret_key = "TLFYHiFq7Cqve+6d/1Vhq88GrnEbFcNph4YtekXz"
 }
 
-terraform {
+/* terraform {
   backend "s3" {
     bucket = "834277767436-tf-state"
     key    = "S3/s3.tfstate"
     region = "us-east-1"
+    access_key = "AKIARQSGNPPR5RAS6KUL"
+    secret_key = "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"
   }
-} 
+}  */
