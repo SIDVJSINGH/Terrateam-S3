18214734a07f5c9fbd9b61b51deeafae2561a154
SIDVJSINGH <sidvjsingh@gmail.com>
2023-09-09 17:00:43 +0530

    initial file

--- AWS-S3/backend.tf
+++ AWS-S3/backend.tf
@@ -8,14 +8,4 @@ terraform {
 
 provider "aws" {
   region = "ap-northeast-1"
-  access_key = "AKIARQSGNPPR5RAS6KUL"
-  secret_key = "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"
 }
-
-/* terraform {
-  backend "s3" {
-    bucket = "834277767436-tf-state"
-    key    = "S3/s3.tfstate"
-    region = "us-east-1"
-  }
-}  */
